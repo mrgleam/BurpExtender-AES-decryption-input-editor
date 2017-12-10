b3625a933e0861c2bede58ce1d4c40e0a7b224fc
Santi Lertsumran <santi@inslash.com>
2017-12-11 00:39:28 +0700

    first commit

---
+++ BurpExtender.java
@@ -0,0 +1,204 @@
+package burp;
+
+import java.awt.Component;
+import java.util.List;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+public class BurpExtender implements IBurpExtender, IMessageEditorTabFactory
+{
+    private IBurpExtenderCallbacks callbacks;
+    private IExtensionHelpers helpers;
+    private static SecretKeySpec secretKey;
+    private static byte[] key;
+    private final String secretKey = "thefuckingkey";
+
+    //
+    // implement IBurpExtender
+    //
+
+    @Override
+    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks)
+    {
+        // keep a reference to our callbacks object
+        this.callbacks = callbacks;
+
+        // obtain an extension helpers object
+        helpers = callbacks.getHelpers();
+
+        // set our extension name
+        callbacks.setExtensionName("AES decryption input editor");
+
+        // register ourselves as a message editor tab factory
+        callbacks.registerMessageEditorTabFactory(this);
+    }
+
+    //
+    // implement IMessageEditorTabFactory
+    //
+
+    @Override
+    public IMessageEditorTab createNewInstance(IMessageEditorController controller, boolean editable)
+    {
+        // create a new instance of our custom editor tab
+        return new Base64InputTab(controller, editable);
+    }
+
+    //
+    // class implementing IMessageEditorTab
+    //
+
+    class Base64InputTab implements IMessageEditorTab
+    {
+        private boolean editable;
+        //private IMessageEditorController controller;
+        private ITextEditor txtInput;
+        private byte[] currentMessage;
+
+        public void setKey(String myKey)
+        {
+            MessageDigest sha = null;
+            try {
+                key = myKey.getBytes("UTF-8");
+                sha = MessageDigest.getInstance("SHA-1");
+                key = sha.digest(key);
+                key = Arrays.copyOf(key, 16);
+                secretKey = new SecretKeySpec(key, "AES");
+            }
+            catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+            catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public String encrypt(String strToEncrypt, String secret)
+        {
+            try
+            {
+                setKey(secret);
+                Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+                cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+                return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error while encrypting: " + e.toString());
+            }
+            return null;
+        }
+
+        public String decrypt(String strToDecrypt, String secret)
+        {
+            try
+            {
+                setKey(secret);
+                Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+                cipher.init(Cipher.DECRYPT_MODE, secretKey);
+                return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error while decrypting: " + e.toString());
+            }
+            return null;
+        }
+
+        public Base64InputTab(IMessageEditorController controller, boolean editable)
+        {
+            this.editable = editable;
+
+            // create an instance of Burp's text editor, to display our deserialized data
+            txtInput = callbacks.createTextEditor();
+            txtInput.setEditable(editable);
+        }
+
+        //
+        // implement IMessageEditorTab
+        //
+
+        @Override
+        public String getTabCaption()
+        {
+            return "AES decryption input";
+        }
+
+        @Override
+        public Component getUiComponent()
+        {
+            return txtInput.getComponent();
+        }
+
+        @Override
+        public boolean isEnabled(byte[] content, boolean isRequest)
+        {
+              if (isRequest) {
+                return true;
+              } else {
+                return true;
+              }
+        }
+
+        @Override
+        public void setMessage(byte[] content, boolean isRequest)
+        {
+            if (content == null)
+            {
+                // clear our display
+                txtInput.setText(null);
+                txtInput.setEditable(false);
+            }
+            else
+            {
+                IRequestInfo info = helpers.analyzeRequest(content);
+                List<String> headers = info.getHeaders();
+                byte[] body = Arrays.copyOfRange(content, info.getBodyOffset(), content.length);
+                String encryptedString = new String(body);
+                String decryptedString = decrypt(encryptedString, secretKey);
+                txtInput.setText(helpers.buildHttpMessage(headers, decryptedString.getBytes()));
+                txtInput.setEditable(editable);
+            }
+
+            // remember the displayed content
+            currentMessage = content;
+        }
+
+        @Override
+        public byte[] getMessage()
+        {
+            // determine whether the user modified the deserialized data
+            if (txtInput.isTextModified())
+            {
+                // reserialize the data
+                byte[] text = txtInput.getText();
+                IRequestInfo info = helpers.analyzeRequest(text);
+                List<String> headers = info.getHeaders();
+                byte[] body = Arrays.copyOfRange(text, info.getBodyOffset(), text.length);
+                String decryptedString = new String(body);
+                String encryptedString = encrypt(decryptedString, secretKey);
+                byte[] input = encryptedString.getBytes();
+                return helpers.buildHttpMessage(headers, input);
+            }
+            else return currentMessage;
+        }
+
+        @Override
+        public boolean isModified()
+        {
+            return txtInput.isTextModified();
+        }
+
+        @Override
+        public byte[] getSelectedData()
+        {
+            return txtInput.getSelectedText();
+        }
+    }
+}
